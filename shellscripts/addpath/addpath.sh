503b8d8c75a2b64f26ed7d2aeef284cc527b7b40
Daniel Fishman <fishdan@yahoo.com>
2025-03-31 11:33:31 -0400

    source .bashrc immediately

--- shellscripts/addpath/addpath.sh
+++ shellscripts/addpath/addpath.sh
@@ -7,7 +7,8 @@ CURRENT_DIR=$(pwd)
 if [[ ":$PATH:" != *":$CURRENT_DIR:"* ]]; then
     echo "Adding $CURRENT_DIR to PATH in ~/.bashrc"
     echo "export PATH=\"\$PATH:$CURRENT_DIR\"" >> ~/.bashrc
-    echo "Done! Run 'source ~/.bashrc' to apply changes."
+    echo "Sourcing ~/.bashrc to apply changes..."
+    source ~/.bashrc
 else
     echo "$CURRENT_DIR is already in your PATH."
 fi
