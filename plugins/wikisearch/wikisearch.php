dcd2447e7cedfdf8e6df42df6bc38604f02a853a
fishdan <dan@reelreport.com>
2025-09-11 22:31:37 -0400

    Bump to 0.1.1

--- plugins/wikisearch/wikisearch.php
+++ plugins/wikisearch/wikisearch.php
@@ -1,12 +1,24 @@
 <?php
 /*
-Plugin Name: WikiSearch Links
-Plugin URI: https://yourwebsite.com/wikisearch-links
+Plugin Name: WikiSearch
+Plugin URI: https://fishdan.com/wikisearch
 Description: Transforms specified hyperlinks into interactive elements for Wikipedia searches.
-Version: 1.0
+Version: 0.1.1
 Author: Dan Fishman
-Author URI: https://yourwebsite.com
+Author URI: https://fishdan.com
 */
+
+/* Optional but recommended so WP knows updates come from GitHub */
+/* Update URI: https://github.com/fishdan/wikisearch */
+
+
+require __DIR__ . '/plugin-update-checker/plugin-update-checker.php';
+$wks_puc = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/fishdan/wikisearch',
+    __FILE__,
+    'wikisearch'
+);
+$wks_puc->getVcsApi()->enableReleaseAssets();
 
 // Enqueue the JavaScript and CSS files
 function wikisearch_enqueue_scripts() {
